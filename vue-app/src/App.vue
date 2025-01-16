ff225f79b164cf64c524d2d24363cef84064c0f5
Kan5661 <linkan0718@gmail.com>
2025-01-16 18:12:22 -0500

    .

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,7 +1,34 @@
 <template>
-  <div>Printer App.vue</div>
+  <div class="AppContainer">
+    <div class="UploadContainer">
+
+    </div>
+    <div class="FilesContainer">
+
+    </div>
+  </div>
 </template>
 
 <script setup>
 
+
 </script>
+
+<style scoped>
+  .AppContainer {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .UploadContainer {
+    width: 100vw;
+    height: 70vh;
+    background-color: grey;
+  }
+
+  .FilesContainer {
+    width: 100vw;
+    height: 50vh;
+    background-color: lightgrey;
+  }
+</style>
